e8d5c0470674fc287e6757a5dae5b07802bce9fe
Felix Harrison <fmlharrison@gmail.com>
2017-01-10 13:08:51 +0000

    passing the third test

--- test/my-app.test.js
+++ test/my-app.test.js
@@ -15,12 +15,23 @@ describe('Sample web app', function() {
   });
 
   describe('when requested at /hello', function() {
-    it('should say hello', function() {
+    it('should say hello', function(done) {
       request.get(baseUrl + '/hello').end(function assert(err, res) {
         expect(err).to.not.be.ok; //expecting no error
         expect(res).to.have.property('status', 200); //expecting to have a status code of 200.
         expect(res.text).to.equal('Hello world!'); //expecting the response to have text that reads 'Hello world!'
         done(); //This is a callback to be called when the request is done.
+      });
+    });
+  });
+
+  describe('when requested at /greetings', function() {
+    it('should greet us', function(done) {
+      request.get(baseUrl + '/greetings').end(function assert(err, res) {
+        expect(err).to.not.be.ok;
+        expect(res).to.have.property('status', 200);
+        expect(res.text).to.match(/Greetings from.*?, and.*?!/i); //Using regex to test for a greeting in the response text.
+        done();
       });
     });
   });
